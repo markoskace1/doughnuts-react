dbb895428319a6fb9d42de2c98c432ba3c3a5fc0
Marko <marko.skace@tutamail.com>
2022-04-27 19:07:41 +0200

    Updating Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex flex-col items-center -mt-12 py-6 bg-slate-800">
+    <div className="flex flex-col items-center -mt-12 py-6 bg-slate-800 text-slate-400">
       <div>
         <a href="https://imgflip.com/i/6e29t8">
           <img
@@ -12,12 +12,10 @@ function Footer() {
         </a>
       </div>
 
-      <a
-        href="https://www.freepik.com/vectors/sweet-bakery"
-        className="mt-4 text-slate-400"
-      >
+      <a href="https://www.freepik.com/vectors/sweet-bakery" className="mt-4">
         Sweet bakery vector created by smithytomy - www.freepik.com
       </a>
+      <p>&copy; Mitja</p>
     </div>
   );
 }
